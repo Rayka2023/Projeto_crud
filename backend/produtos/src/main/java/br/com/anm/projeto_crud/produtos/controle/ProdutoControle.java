dd23af8e1abcf4bbc9f3fd50c18663f428996e5a
Rayka <rayka.simplicio@aluno.ce.gov.br>
2025-03-18 11:45:01 -0300

    Criação da rota remover

--- backend/produtos/src/main/java/br/com/anm/projeto_crud/produtos/controle/ProdutoControle.java
+++ backend/produtos/src/main/java/br/com/anm/projeto_crud/produtos/controle/ProdutoControle.java
@@ -1,24 +1,53 @@
 package br.com.anm.projeto_crud.produtos.controle;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.anm.projeto_crud.produtos.modelo.ProdutoModelo;
+import br.com.anm.projeto_crud.produtos.modelo.RespostaModelo;
+import br.com.anm.projeto_crud.produtos.repositorio.ProdutoRepositorio;
 import br.com.anm.projeto_crud.produtos.servico.ProdutoServico;
 
 @RestController
 public class ProdutoControle {
 
+    private final ProdutoRepositorio produtoRepositorio;
+
     @Autowired
     private ProdutoServico ps;
+
+    ProdutoControle(ProdutoRepositorio produtoRepositorio) {
+        this.produtoRepositorio = produtoRepositorio;
+    }
+ 
+    @PostMapping("/cadastrar")
+    public ResponseEntity<?> cadastrar(@RequestBody ProdutoModelo pm){
+        return ps.cadastrarAlterar(pm, "cadastrar");
+    }
+    
+    @PutMapping("/alterar")
+    public ResponseEntity<?> alterar(@RequestBody ProdutoModelo pm){
+        return ps.cadastrarAlterar(pm, "alterar");
+    }
 
     @GetMapping("/listar")
     public Iterable<ProdutoModelo> listar(){
         return ps.listar();
     }
-    
-    @GetMapping("/")
+
+    @DeleteMapping("/remover/{codigo}")
+    public ResponseEntity<RespostaModelo> remover(@PathVariable long codigo){
+        return ps.remover(codigo);
+    }
+
+     @GetMapping("/")
     public String rota(){
         return "A API está funcionando!";
     }
